7f79737387c921280faeea5db9b6e2bc06afdf9c
AsfaaKhan <159608757+AsfaaKhan@users.noreply.github.com>
2024-05-26 00:16:13 -0700

    Update msg.ts

--- 02_Personal Message/msg.ts
+++ 02_Personal Message/msg.ts
@@ -6,20 +6,7 @@
 
 let personName :string = "Asfaa Khan";
 
-console.log(`"${personName} , would you like to learn some typescript today?"`);
+console.log(`"${personName}, would you like to learn some typescript today?"`);
 
 
 
- 
-// //SELF PRACTICE
-// let personName2 :string= "";
-//  personName2  = prompt("Write your name .") || " ";
-
-// if (personName2 !== null && personName2 !== ' '){
-//     alert(`Hi ${personName2}, "would you like to learn some typescript?" `);
-    
-// }
-// else{
-//     alert("Firstly you have to fill your name! ");
-    
-// }
